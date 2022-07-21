9b21f2074da2892a776659a39774f76dd5212653
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-07-21 19:10:48 -0300

    chore: coordinates move logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Running');
+let finalPosition: number[];
+let currentPosition: number[];
+
+function changePosition(initialPosition: number[], vector: [string, number]) {
+  currentPosition = initialPosition;
+
+  const acceptedDirection = {
+    N(): number[] {
+      return currentPosition = [currentPosition[0], currentPosition[1] + vector[1]];
+    },
+    S(): number[] {
+      return currentPosition = [currentPosition[0], currentPosition[1] - vector[1]];
+    },
+    L(): number[] {
+      return currentPosition = [currentPosition[0] + vector[1], currentPosition[1]];
+    },
+    O(): number[] {
+      return currentPosition = [currentPosition[0] - vector[1], currentPosition[1]];
+    }
+  }
+}
